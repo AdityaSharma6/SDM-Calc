cfb95e8ca1a3b0f2c42f15ad4143b7f9ba2b4dcf
Aditya Sharma <shara24@mcmaster.ca>
2020-08-19 17:10:30 -0400

    Reboot

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Card } from "./components/Card";
-
-ReactDOM.render(
-    <Card/>,
-    document.getElementById("root")
-)
