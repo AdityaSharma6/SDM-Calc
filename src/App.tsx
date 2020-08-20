cf63e1c5dd31e09770ce987a2302763a2507ccfc
Aditya Sharma <shara24@mcmaster.ca>
2020-08-20 00:37:41 -0400

    Prettier Setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Hello World I'm testing the prettier functionality by writing a super long line that should be reformatted. hopefully. lets see what it does
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
