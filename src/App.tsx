4705750c7fc9b2fa769ee9d282d66b651f084b71
Aditya Sharma <shara24@mcmaster.ca>
2020-08-20 00:27:00 -0400

    Setup Files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World I'm testing the prettier functionality by writing a super long line that should be reformatted. hopefully. lets see what it does
+    </div>
+  );
+}
+
+export default App;
